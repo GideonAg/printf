b9286d14fbf09a3224fffd5c57f82272a72e36e5
Joseph Ampadu <jkampadujnr@gmail.com>
2022-11-20 14:54:09 +0000

    fix betty errors (#4)

--- _printf.c
+++ _printf.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
- * _print - write output to stdout according to a format
+ * _printf - write output to stdout according to a format
  * @format: specifies format in which output should be in
  * Return: number of characters printed to stdout.
  * Has a negative return value if format is NULL
  */
-
 int _printf(const char *format, ...)
 {
 	int len;
 	va_list arg;
-	va_start(arg, format);
 
 	if (format == NULL)
 		return (-1);
 
+	va_start(arg, format);
 	len = controller(format, arg);
+	va_end(arg);
 
 	return (len);
 }
